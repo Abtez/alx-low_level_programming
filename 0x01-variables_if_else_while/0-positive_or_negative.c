9cca9a380c9fddf78ee26ccc04c6c22c086e4b24
Abzed Abdul Mohammed <ibraabzed@gmail.com>
2022-03-12 21:46:07 +0300

    pos & neg fix

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,7 +1,13 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 /* more headers goes there */
-
+/**
+ * main - main block
+ * Description: Get a random number and print the number
+ * and if it is positive, negative, or zero
+ * Return: 0
+ */
 /* betty style doc for function main goes there */
 int main(void)
 {
@@ -11,11 +17,11 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
     if (n > 0)
-        printf("%i is positive\n", n);
-    else
-        if (n < 0)
+            printf("%i is positive\n", n);
+    else if (n < 0)
             printf("%i is negative\n", n);
-        else
+    else
             printf("%i is zero\n", n);
+        
 	return (0);
 }
