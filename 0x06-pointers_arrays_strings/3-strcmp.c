ebb5f5c4d4c1142af1ac31a6df79d841d20c2aaf
Abzed Abdul Mohammed <ibraabzed@gmail.com>
2022-03-23 19:39:10 +0300

    fix3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -15,10 +15,10 @@ int i;
 */
 for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
 {
-	if (s1[i] != s2[i])
-	{
-		return (s1[i] - s2[i]);
-	}
+if (s1[i] != s2[i])
+{
+return (s1[i] - s2[i]);
+}
 }
 return (0);
 }
